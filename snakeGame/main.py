c09b92663981e3976f5d0da26dbba4bdba428092
Aatreus01 <beraterhansekeroz@gmail.com>
2024-03-16 13:20:19 +0300

    yılan artık koşuyor

--- snakeGame/main.py
+++ snakeGame/main.py
@@ -10,6 +10,12 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(key="w", fun=snake.up)
+screen.onkey(key="s", fun=snake.down)
+screen.onkey(key="d", fun=snake.right)
+screen.onkey(key="a", fun=snake.left)
+
 game_is_on = True
 while game_is_on:
     screen.update()
