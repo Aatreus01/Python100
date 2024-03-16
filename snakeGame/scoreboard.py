08cb04bb50c2b656937e0dc0a9322f0b46517602
Aatreus01 <beraterhansekeroz@gmail.com>
2024-03-16 18:08:19 +0300

    completed score function

--- snakeGame/scoreboard.py
+++ snakeGame/scoreboard.py
@@ -1,11 +1,23 @@
 from turtle import Turtle
 
+ALIGNMENT = "center"
+FONT = ("Comic Sans", 15, "normal")
+
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        score = 0
+        self.score = 0
         self.pu()
         self.hideturtle()
         self.goto(0, 270)
         self.color("white")
-        self.write(f"Score: {score}", align="center", font=("Comic Sans", 20, "normal"))
+        self.update_scoreboard()
+    
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font= FONT)
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font= FONT)
