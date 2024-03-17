e211533ea2610223c8b3351c16d45e286167a6bc
Aatreus01 <beraterhansekeroz@gmail.com>
2024-03-17 17:52:15 +0300

    created Ball class

--- pongGame/main.py
+++ pongGame/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 import paddle
+import ball
 
 screen = Screen()
 screen.bgcolor("black")
@@ -12,15 +13,20 @@ pos_left = (-350, 0)
 paddle_r = paddle.Paddle(pos_right)
 paddle_l = paddle.Paddle(pos_left)
 
+ball = ball.Ball()
+
+
+
 game_is_on = True
 while game_is_on:
     screen.update()
+    ball.move()
     screen.listen()
     screen.onkey(key="w", fun=paddle_l.moveUp)
     screen.onkey(key="s", fun=paddle_l.moveDown)
     screen.onkey(key="Up", fun=paddle_r.moveUp)
     screen.onkey(key="Down", fun=paddle_r.moveDown)
-
+    
 
 
 
