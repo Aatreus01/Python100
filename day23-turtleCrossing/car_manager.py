96c74770cb1cddae4b07d181aecdb97d4181a187
Aatreus01 <beraterhansekeroz@gmail.com>
2024-03-19 22:40:09 +0300

    cars spawn on the right edge of screen now

--- day23-turtleCrossing/car_manager.py
+++ day23-turtleCrossing/car_manager.py
@@ -1,7 +1,24 @@
+from turtle import Turtle
+from random import randint
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.pu()
+        random_y = randint(-250, 250)
+        self.setpos(300, random_y)
+        self.setheading(180)
+        self.shapesize(stretch_len=2, stretch_wid=1)
+        self.random_color()
 
-class CarManager:
-    pass
+    def move_car(self):
+        self.forward(STARTING_MOVE_DISTANCE)
+
+    def random_color(self):
+        x = randint(0, len(COLORS) - 1)
+        self.color(COLORS[x])
+        
--- day23-turtleCrossing/main.py
+++ day23-turtleCrossing/main.py
@@ -3,17 +3,28 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
+import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
+cars = []
+
 
 screen.listen()
 screen.onkey(key="w", fun=player.move)
+
+count = 5
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
+    if count == 5:
+        cars.append(CarManager())
+        count = 0
+    
+    for car in cars:
+        car.move_car()
+    count += 1
