f1f4a0efe578a0ed958998922464b85dd60cf9fc
Aatreus01 <beraterhansekeroz@gmail.com>
2024-03-20 02:09:20 +0300

    cars now speed up on level advance

--- day23-turtleCrossing/car_manager.py
+++ day23-turtleCrossing/car_manager.py
@@ -10,10 +10,11 @@ class CarManager(Turtle):
         self.shape("square")
         self.pu()
         self.generate_car()
+        self.speed = STARTING_MOVE_DISTANCE
 
     def generate_car(self):
         random_y = randint(-250, 250)
-        self.setpos(300, random_y)
+        self.setpos(310, random_y)
         self.setheading(180)
         self.shapesize(stretch_len=2, stretch_wid=1)
         self.random_color()
@@ -22,5 +23,15 @@ class CarManager(Turtle):
         x = randint(0, len(COLORS) - 1)
         self.color(COLORS[x])
         
+    def reset_pos(self):
+        self.setpos(310, self.random_y)
+        
+    def set_speed(self, level):
+        self.speed = (level*MOVE_INCREMENT) + STARTING_MOVE_DISTANCE   
+        
     def move_car(self):
-        self.forward(STARTING_MOVE_DISTANCE)
+        random_y = randint(-250, 250)
+        if self.xcor() < -320:
+            self.goto(310, random_y)
+        
+        self.forward(self.speed)
--- day23-turtleCrossing/main.py
+++ day23-turtleCrossing/main.py
@@ -16,15 +16,25 @@ screen.listen()
 screen.onkey(key="w", fun=player.move)
 
 count = 5
+level = 0
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    if count == 5:
-        cars.append(CarManager())
+    if player.detect_win():
+        level += 1
+    if count == 5 and len(cars) < 40:
+        cars.append(CarManager())   # Spawn a car every 6th step of the loop
         count = 0
-    
     for car in cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            break
+        car.set_speed(level) # Move the cars and detect collision
         car.move_car()
+    
     count += 1
+
+
+screen.exitonclick()
