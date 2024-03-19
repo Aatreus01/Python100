ebc5b9fbb54c5bebc37ab115dbde8b78fb313c95
Aatreus01 <beraterhansekeroz@gmail.com>
2024-03-20 02:26:32 +0300

    tweaks to use scoreboard

--- day23-turtleCrossing/main.py
+++ day23-turtleCrossing/main.py
@@ -3,14 +3,13 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
-import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
+scoreboard = Scoreboard()
 cars = []
-
 
 screen.listen()
 screen.onkey(key="w", fun=player.move)
@@ -23,17 +22,17 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     if player.detect_win():
-        level += 1
+        scoreboard.update_scoreboard()
     if count == 5 and len(cars) < 40:
         cars.append(CarManager())   # Spawn a car every 6th step of the loop
         count = 0
     for car in cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
             break
-        car.set_speed(level) # Move the cars and detect collision
+        car.set_speed(scoreboard.level - 1) # Move the cars and detect collision
         car.move_car()
-    
     count += 1
 
 
