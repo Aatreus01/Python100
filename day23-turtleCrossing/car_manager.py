17a4b28047206b992f4d319b441525b4120f5e78
Aatreus01 <beraterhansekeroz@gmail.com>
2024-03-19 22:41:56 +0300

    some tweaks to CarManager

--- day23-turtleCrossing/car_manager.py
+++ day23-turtleCrossing/car_manager.py
@@ -9,16 +9,18 @@ class CarManager(Turtle):
         super().__init__()
         self.shape("square")
         self.pu()
+        self.generate_car()
+
+    def generate_car(self):
         random_y = randint(-250, 250)
         self.setpos(300, random_y)
         self.setheading(180)
         self.shapesize(stretch_len=2, stretch_wid=1)
         self.random_color()
-
-    def move_car(self):
-        self.forward(STARTING_MOVE_DISTANCE)
-
+    
     def random_color(self):
         x = randint(0, len(COLORS) - 1)
         self.color(COLORS[x])
         
+    def move_car(self):
+        self.forward(STARTING_MOVE_DISTANCE)
