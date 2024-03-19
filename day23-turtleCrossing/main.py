2e11572e83e40e743a915573007c2fa17ab06cf2
Aatreus01 <beraterhansekeroz@gmail.com>
2024-03-19 22:11:21 +0300

    player can move now

--- day23-turtleCrossing/main.py
+++ day23-turtleCrossing/main.py
@@ -7,8 +7,10 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-
 player = Player()
+
+screen.listen()
+screen.onkey(key="w", fun=player.move)
 
 game_is_on = True
 while game_is_on:
