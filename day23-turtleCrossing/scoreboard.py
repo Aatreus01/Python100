0e144c0d507efb4d8f35452c2e48b5ddc4dfc73e
Aatreus01 <beraterhansekeroz@gmail.com>
2024-03-20 02:26:10 +0300

    added level and gameover functionality

--- day23-turtleCrossing/scoreboard.py
+++ day23-turtleCrossing/scoreboard.py
@@ -1,5 +1,23 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+FONT = ("Courier", 20, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.level = 0
+        self.color("black")
+        self.pu()
+        self.hideturtle()
+        self.update_scoreboard()
+        
+    def update_scoreboard(self):
+        self.level += 1
+        self.clear()
+        self.goto(-290, 270)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+        
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=FONT)
