35af1b95e6e34ad28677f12362f72a6ce3ff51f0
Aatreus01 <beraterhansekeroz@gmail.com>
2024-04-20 21:07:56 +0300

    completed the task in one go lmao

---
+++ Mail Merge Project Start/main.py
@@ -0,0 +1,20 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readnames.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+        
+with open('..\\Mail Merge Project Start\\Input\\Names\\invited_names.txt') as f:
+    names = [line.rstrip() for line in f]
+    
+print(names)
+
+for name in names:
+    with open('..\\Mail Merge Project Start\\Input\\Letters\\starting_letter.txt') as f:
+        content = f.read()
+        content = content.replace("[name]", name) 
+    with open(f"..\\Mail Merge Project Start\\Output\\ReadyToSend\\letter_for_{name}.txt", mode="w") as f:
+        f.write(content)
