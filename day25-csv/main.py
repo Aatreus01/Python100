6428dee4292cf3fa759d06b490358f2836adb1ad
Aatreus01 <beraterhansekeroz@gmail.com>
2024-04-23 19:42:59 +0300

    lel

--- day25-csv/main.py
+++ day25-csv/main.py
@@ -13,3 +13,17 @@
 #print(temperatures)
 
 import pandas
+'''
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
+temp_list = data["temp"].to_list()
+sum = 0
+max_temp = data["temp"].max()
+print(data[data.temp == max_temp])
+'''
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+print(data)
+
+FurColor = data["Primary Fur Color"].s
