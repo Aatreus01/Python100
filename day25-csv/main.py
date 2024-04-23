a362e9add99af263e124b4f4510c6ac71e363ed8
Aatreus01 <beraterhansekeroz@gmail.com>
2024-04-23 15:51:27 +0300

    idk

---
+++ day25-csv/main.py
@@ -0,0 +1,15 @@
+#with open("..\\day25-csv\\weather_data.csv") as f:
+##data = f.readlines()
+##print(data)
+
+#import csv
+
+#with open("..\\day25-csv\\weather_data.csv") as f:
+#data = csv.reader(f)
+#temperatures = []
+#for row in data:
+#if row[1] != "temp":
+#temperatures.append(int(row[1]))
+#print(temperatures)
+
+import pandas
